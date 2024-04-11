50c2edb7e668e4ad5ef583a327449c5471f0d1ee
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-11 21:38:41 +0800

    feat(api): implement two viewsets for overview
    Currently, two viewsets are the same. Separate classes are implemented for later purpose if different viewing layout affect the data user can see.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from core.models import Category, Molecule
+from core.views import OverviewPagination
+from django.db.models import Prefetch
+from rest_framework import filters, generics, pagination, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import OverviewSerializer
+
+
+# cards grid layout with pagination
+class OverviewCardViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Molecule.objects.all()
+    serializer_class = OverviewSerializer
+    pagination_class = OverviewPagination
+
+
+# table layout with pagination
+class OverviewTableView(viewsets.ReadOnlyModelViewSet):
+    queryset = Molecule.objects.all()
+    serializer_class = OverviewSerializer
+    pagination_class = OverviewPagination
