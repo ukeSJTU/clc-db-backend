16c86b15a0205ca03d542aa39a24350f483320c8
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-11 21:36:10 +0800

    feat(core): :sparkles: implement pagination
    Initialize pagination setting in core for reuse.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import pagination
 
-# Create your views here.
+
+# custom pagination settings for overview api
+class OverviewPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
