61f1b30ea1ee36a75874a5a279f57980ff919c42
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-11 21:08:03 +0800

    feat(core): register models in core to admin
    Register three models in core databse: Category, Smile and Molecule to admin site.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Category, Molecule, Smile
+
 # Register your models here.
+admin.site.register(Molecule)
+admin.site.register(Category)
+admin.site.register(Smile)
