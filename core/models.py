050d51e016dc6ff9faa20d2bb7cad5d427ac08b1
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-11 21:06:32 +0800

    feat(core): :sparkles: implement Molecule class to represent a molecule in db

--- core/models.py
+++ core/models.py
@@ -15,3 +15,29 @@ class Smile(models.Model):
 
     def __str__(self):
         return self.smile
+
+
+class Molecule(models.Model):
+    name = models.CharField(max_length=200)  # observe extra long name in given dataset
+    cas_id = models.CharField(max_length=20)  # format: ######-##-#
+    class_type = models.ManyToManyField(
+        Category, related_name="molecules"
+    )  # class type of the molecule, metadata is in chinese format
+
+    url = models.URLField()  # url to the molecule's external information page
+    pubchem_url = models.URLField()  # url to the molecule's pubchem page
+    smiles = models.CharField(max_length=200)  # smiles representation of the molecule
+
+    smiles_type = models.ManyToManyField(
+        Smile, related_name="molecules"
+    )  # type of the smiles representation of the molecule
+
+    remark = models.TextField(
+        blank=True,
+        null=True,
+    )  # [optional] any additional information about the molecule
+
+    def __str__(self):
+        return "Molecule: {} | CAS ID: {} | Class: {}".format(
+            self.name, self.cas_id, self.class_type.all()
+        )
