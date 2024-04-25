f5ae8fb24882e55839dd78d18e56775f2b489409
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-25 08:09:51 +0800

    feat(db): :sparkles: add django command to load data from csv to sqlite database
    To deploy, you need to run the `load_molecules` command under `core` app to load data.

---
+++ core/management/commands/load_molecules.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.core.management.base import BaseCommand
+from core.models import Molecule, Category, Smile
+import pandas as pd
+
+
+class Command(BaseCommand):
+    help = "Load molecule data into the database"
+
+    def handle(self, *args, **kwargs):
+        df = pd.read_csv(
+            "/Users/uke/Downloads/all_data/merged.csv"
+        )  # Load your dataframe as needed
+
+        categories = {
+            name: Category.objects.get_or_create(name=name)[0]
+            for name in df["类别"].unique()
+        }
+        smiles_types = {
+            smile: Smile.objects.get_or_create(smile=smile)[0]
+            for smile in df["手性种类"].unique()
+        }
+
+        for idx, row in df.iterrows():
+            molecule, created = Molecule.objects.update_or_create(
+                cas_id=row["CAS号"],
+                defaults={
+                    "name": row.get("Name", ""),
+                    "pubchem_cid": int(row.get("PubChem CID", "")),
+                    "url": row.get("URL", ""),
+                    "pubchem_url": row.get("PubChemURL", ""),
+                    "smiles": row.get("SMILES", ""),
+                    "smiles_iupac": row.get("SMILES_IUPAC", ""),
+                    "molecule_formula": row.get("MF", ""),
+                    "molecular_weight": float(row.get("MW", 0)),
+                    "remark": row.get("备注", ""),
+                },
+            )
+            # Assign categories and smile types
+            molecule.class_type.set([categories[row["类别"]]])
+            molecule.smiles_type.set([smiles_types[row["手性种类"]]])
+
+            molecule.save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully added/updated molecule: {molecule.name}"
+                )
+            )
