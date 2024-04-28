b48621ee5bb50e16932cd2985c2ecde5efe89296
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-29 02:28:09 +0800

    feat: :sparkles: add pagination setting for download api

--- core/views.py
+++ core/views.py
@@ -7,3 +7,9 @@ class OverviewPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
+
+
+class DownloadClassesPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
