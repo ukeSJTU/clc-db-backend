dcee4889851c61e09d3b61b59d12a11fcc0b714d
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-04-29 02:29:07 +0800

    feat: :sparkles: implement new viewset for download by class types

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from core.models import Category, Molecule
-from core.views import OverviewPagination
+from core.views import OverviewPagination, DownloadClassesPagination
 from django.db.models import Prefetch
 from rest_framework import filters, generics, pagination, viewsets
 from rest_framework.decorators import action
@@ -34,18 +34,35 @@ class OverviewTableView(viewsets.ReadOnlyModelViewSet):
 class DownloadClassesViewSet(viewsets.ModelViewSet):
     queryset = Molecule.objects.all()
     serializer_class = DownloadSerializer
+    pagination_class = DownloadClassesPagination
+
+    @action(detail=False, methods=["get"], url_path="all")
+    def all_classes(self, request):
+        queryset = Molecule.objects.all()
+
+    serializer_class = DownloadSerializer
+    pagination_class = DownloadClassesPagination
 
     @action(detail=False, methods=["get"], url_path="all")
     def all_classes(self, request):
         categories = Category.objects.all()
+        paginated_categories = self.paginate_queryset(
+            categories
+        )  # Apply pagination to categories
         result = []
 
-        for category in categories:
-            molecules = Molecule.objects.filter(class_type=category)
-            serializer = self.get_serializer(molecules, many=True)
-            result.append({"class_type": category.name, "molecules": serializer.data})
+        if paginated_categories is not None:
+            for category in paginated_categories:
+                molecules = Molecule.objects.filter(class_type=category)
+                serializer = self.get_serializer(molecules, many=True)
+                result.append(
+                    {"class_type": category.name, "molecules": serializer.data}
+                )
 
-        return Response(result)
+            return self.get_paginated_response(result)  # Return paginated response
+        else:
+            # Handle the case if pagination is not applicable or error
+            return Response({"error": "Pagination error or no categories found."})
 
 
 # download api based on molecule's cas_id
