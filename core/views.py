3db65c8f414b2df2cb2816e85ef4b7222a341db6
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-12 23:54:22 +0800

    feat(core): :sparkles: add a default pagination class

--- core/views.py
+++ core/views.py
@@ -13,3 +13,9 @@ class DownloadClassesPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
+
+
+class DefaultPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
