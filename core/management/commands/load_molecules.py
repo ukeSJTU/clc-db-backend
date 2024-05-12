da2efaaa4edbed1f62fa9d917413ef6e964fe943
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2024-05-12 23:32:52 +0800

    feat(db): :boom: update model definition

--- core/management/commands/load_molecules.py
+++ core/management/commands/load_molecules.py
@@ -1,6 +1,3 @@
-from django.test import TestCase
-
-# Create your tests here.
 from django.core.management.base import BaseCommand
 from core.models import Molecule, Category, Smile
 import pandas as pd
@@ -10,18 +7,23 @@ class Command(BaseCommand):
     help = "Load molecule data into the database"
 
     fill_values = {
-        "PubChem CID": 0,  # 对于 'PubChem CID' 列，用 0 填充
-        "URL": "",  # 对于 'URL' 列，用空字符串填充
-        "SMILES": "N/A",  # 对于 'SMILES' 列，用 'N/A' 填充
-        "SMILES_IUPAC": "N/A",  # 对于 'SMILES_IUPAC' 列，用 'N/A' 填充
-        "MF": "N/A",  # 对于 'MF' 列，用 'N/A' 填充
-        "MW": 0.0,  # 对于 'MW' 列，用 0 填充
-        "备注": "N/A",  # 对于 '备注' 列，用 'N/A' 填充
-        "手性种类": "无",  # 对于 '手性种类' 列，用 '无' 填充
+        "PubChem CID": 0,
+        "URL": "",
+        "SMILES": "N/A",
+        "SMILES_IUPAC": "N/A",
+        "MF": "N/A",
+        "MW": 0.0,
+        "备注": "N/A",
+        "Heavy Atom Count": 0,
+        "Ring Count": 0,
+        "Hydrogen Bond Acceptor Count": 0,
+        "Hydrogen Bond Donor Count": 0,
+        "Rotatable Bond Count": 0,
+        "Description": "N/A",
+        "types of chirality": "N/A",
     }
 
     def add_arguments(self, parser):
-        # Adding an optional argument to specify the path to the CSV file
         parser.add_argument(
             "-p",
             "--path",
@@ -33,9 +35,9 @@ class Command(BaseCommand):
         csv_path = kwargs["path"]
         df = pd.read_csv(csv_path).fillna(self.fill_values)
 
-        # Create or get categories and smiles types from the dataframe
+        # Create or get categories and smile types from the dataframe
         category_objs = {}
-        for category_list in df["类别"].unique():
+        for category_list in df["Category"].unique():
             categories = [cat.strip() for cat in category_list.split("、")]
             for category in categories:
                 if category not in category_objs:
@@ -43,37 +45,47 @@ class Command(BaseCommand):
                         name=category
                     )
 
-        smile_objs = {}
-        for smile in df["手性种类"].unique():
-            smile_objs[smile], _ = Smile.objects.get_or_create(smile=smile)
+        smile_type_objs = {}
+        for smile_type in df["types of chirality"].unique():
+            smile_type_objs[smile_type], _ = Smile.objects.get_or_create(
+                smile=smile_type
+            )
 
         for idx, row in df.iterrows():
             molecule, created = Molecule.objects.update_or_create(
-                cas_id=row["CAS号"],
+                cas_id=row["CAS"],
                 defaults={
-                    "name": row.get("Name", ""),
-                    "pubchem_cid": str(
-                        int(row.get("PubChem CID", 0))
-                    ),  # Convert to int, if not exist set to 0; then convert to string because PubChem CID is CharField
-                    "url": row.get("URL", ""),
+                    "name": row["Name"],
+                    "pubchem_cid": str(int(row.get("PubChem CID", 0))),
                     "pubchem_url": row.get("PubChemURL", ""),
                     "smiles": row.get("SMILES", ""),
                     "smiles_iupac": row.get("SMILES_IUPAC", ""),
                     "molecule_formula": row.get("MF", ""),
                     "molecular_weight": float(row.get("MW", 0)),
-                    "remark": row.get("备注", ""),
+                    "description": row.get("Description", ""),
+                    "heavy_atom_count": int(row.get("Heavy Atom Count", 0)),
+                    "ring_count": int(row.get("Ring Count", 0)),
+                    "hydrogen_bond_acceptor_count": int(
+                        row.get("Hydrogen Bond Acceptor Count", 0)
+                    ),
+                    "hydrogen_bond_donor_count": int(
+                        row.get("Hydrogen Bond Donor Count", 0)
+                    ),
+                    "rotatable_bond_count": int(row.get("Rotatable Bond Count", 0)),
                 },
             )
-            # Assign categories and smile types
-            # molecule.class_type.set([categories[row["类别"]]])
-            categories = [cat.strip() for cat in row["类别"].split("、")]
+
+            categories = [cat.strip() for cat in row["Category"].split("、")]
             molecule.class_type.set(
                 [category_objs[cat] for cat in categories if cat in category_objs]
             )
-            molecule.smiles_type.set([smile_objs[row["手性种类"]]])
-            # molecule.smiles_type.set([smiles_types[row["手性种类"]]])
+
+            smile_type = row.get("types of chirality")
+            if smile_type in smile_type_objs:
+                molecule.chirality.set([smile_type_objs[smile_type]])
 
             molecule.save()
+
             self.stdout.write(
                 self.style.SUCCESS(
                     f"Successfully added/updated molecule: {molecule.name}"
--- core/models.py
+++ core/models.py
@@ -22,65 +22,50 @@ class Molecule(models.Model):
     SMILE_MAX_LENGTH = 200
     MOLECULE_FORMULA_MAX_LENGTH = 100
 
-    # name of the molecule: 3-(tert-Butyl)-4-(2,6-dimethoxyphenyl)-2,3-dihydrobenzo[d][1,3]oxaphosphole
     name = models.CharField(
         max_length=IUPAC_NAME_MAX_LENGTH
     )  # explicitly set max_length to 200 in case of long names
 
-    # cas_id of the molecule: 1246888-90-3
     cas_id = models.CharField(max_length=20, unique=True)  # this should be unique
 
-    # pubchem_cid of the molecule: 46939810
-    pubchem_cid = models.CharField(
-        max_length=20, blank=True, null=True
-    )  # this should be unique
-
-    # class type of the molecule: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     class_type = models.ManyToManyField(
         Category, related_name="molecules"
     )  # class type of the molecule, metadata is in chinese format
 
-    # url to the molecule's external information page, could be empty, if not available default is empty string
-    url = models.URLField(
-        blank=True, null=True
-    )  # https://www.bidepharm.com/products/1246888-90-3.html
-
-    # url to the molecule's pubchem page, could be empty, if not available default is empty string
     pubchem_url = models.URLField(
         blank=True, null=True
     )  # https://pubchem.ncbi.nlm.nih.gov/compound/46939810
 
-    # smiles representation of the molecule: CC(C)(C)P1COC2=CC=CC(=C21)C3=C(C=CC=C3OC)OC
     smiles = models.CharField(max_length=SMILE_MAX_LENGTH)
 
-    # smile type of the smiles representation of the molecule: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    smiles_type = models.ManyToManyField(
+    chirality = models.ManyToManyField(
         Smile, related_name="molecules"
     )  # smile type of the molecule, metadata is in chinese format
 
-    # iupac name of the molecule: 3-tert-butyl-4-(2,6-dimethoxyphenyl)-2H-1,3-benzoxaphosphole
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )  # [optional] any additional information about the molecule
+
+    pubchem_cid = models.CharField(
+        max_length=20, blank=True, null=True
+    )  # this should be unique
+
     smiles_iupac = models.CharField(
         max_length=IUPAC_NAME_MAX_LENGTH, blank=True, null=True
     )  # IUPAC name of the molecule
 
-    # molecular formula of the molecule: C19H23O3P
-    # TODO: remember to change blank=False later on
     molecule_formula = models.CharField(
         max_length=MOLECULE_FORMULA_MAX_LENGTH,
         blank=True,
         null=True,
     )  # C19H23O3P
 
-    # molecular weight of the molecule: 330.364
-    # TODO: remember to change blank=False later on
     molecular_weight = models.FloatField(blank=True, null=True)  # 330.364
 
-    remark = models.TextField(
-        blank=True,
-        null=True,
-    )  # [optional] any additional information about the molecule
-
-    def __str__(self):
-        return "Molecule: {} | CAS ID: {} | Class: {}".format(
-            self.name, self.cas_id, self.class_type.all()
-        )
+    # Calculated statistics
+    heavy_atom_count = models.IntegerField(blank=True, null=True)  # 23
+    ring_count = models.IntegerField(blank=True, null=True)  # 2
+    hydrogen_bond_acceptor_count = models.IntegerField(blank=True, null=True)  # 3
+    hydrogen_bond_donor_count = models.IntegerField(blank=True, null=True)  # 0
+    rotatable_bond_count = models.IntegerField(blank=True, null=True)  # 2
